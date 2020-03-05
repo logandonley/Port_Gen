b6fdf7bb2484f485033cfe1005ad9ef2138e3d8d
johns2te <johns2te@gmail.com>
2020-03-05 10:32:57 -0500

    Update internet.bat

--- internet.bat
+++ internet.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Checking connection, please wait...
 PING 194.109.6.66 | FIND "Reply from " > NUL
-IF NOT ERRORLEVEL 1 ECHO You have an active connection to the internet
-IF     ERRORLEVEL 1 ECHO You have NO active connection to the internet
+IF NOT ERRORLEVEL EXIT 0
+IF     ERRORLEVEL 1 EXIT 1
