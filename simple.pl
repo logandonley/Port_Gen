8d42e04efa913f3ade8577ca4c90b0f2124ad4b0
johns2te <johns2te@gmail.com>
2020-03-05 10:37:49 -0500

    Update simple.pl

--- simple.pl
+++ simple.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 
 my $output = `internet.bat`;
+if($output = 0, printf("Internet Status: Connected", printf("Internet Status: Disconnected");
 
-printf("Internet Status:", $output);
+
