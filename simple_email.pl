6cbfaf5a1dca9b637e9349f5f5c674fd9dee79e0
johns2te <johns2te@gmail.com>
2020-03-05 10:16:48 -0500

    Update simple_email.pl

--- simple_email.pl
+++ simple_email.pl
@@ -1,22 +1,3 @@
 #!/usr/bin/perl
-use strict;
-use warnings;
 
-# first, create your message
-use Email::MIME;
-my $message = Email::MIME->create(
-  header_str => [
-    From    => 'johns2te@gmail.com',
-    To      => 'tjohnson@cloudbees.com',
-    Subject => 'Welcome To CloudBees!',
-  ],
-  attributes => {
-    encoding => 'quoted-printable',
-    charset  => 'ISO-8859-1',
-  },
-  body_str => "CloudBees!\n",
-);
-
-# send the message
-use Email::Sender::Simple qw(sendmail);
-sendmail($message);
+system 'internet.batch'
